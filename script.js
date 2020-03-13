d560c68b9444e23294dc6a6c53366a32c6a1c9dc
Filin <v.proskurkin.exceedteam@gmail.com>
2020-03-13 18:19:39 +0300

    update card files

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-
+function getRandom(max) {
+	return Math.floor(Math.random() * max);
+}
 $("#but").on('click', function(){
-	// const input_Check = $(input).attr('type','checkbox');
 	
-	$('#list').append('<li><input class="class_input_checkbox" type="checkbox"><span class="class_span">This is the text in new element. (with jQuery)</span></li>');
+	var text = $('.getting_text').val();
+	$('#list').append('<li><div class="cl1"><input class="class_input_checkbox" type="checkbox"></div><span></span></li>');
+	$('span').text(text);
 });
+let a = getRandom(4);
+
+alert(a);
+// $(function (){
+  
+// 		$('#but').click(function(){
+	
+// 		   var text = $('.getting_text').val();
+// 		   $('#list').append('<li><div class="cl1"><input class="class_input_checkbox" type="checkbox"></div><span class="class_span"></span></li>');
+// 		   $('span').text(text);
+			
+// 		});
+	
+// 	});
+// $(function (){
+	
+// 		$('but').click(function(){
+	
+// 		   var text = $('getting_text').val();
+	
+// 		   $('dv').text(text);
+	
+// 		});
+	
+// 	});
+	
