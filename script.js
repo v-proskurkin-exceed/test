edb43c8291eca0e306e6d55b2fccbdb9c86ff710
Filin <v.proskurkin.exceedteam@gmail.com>
2020-03-13 13:37:56 +0300

    Style creation for jquery app

---
+++ script.js
@@ -0,0 +1,6 @@
+
+$("#but").on('click', function(){
+	// const input_Check = $(input).attr('type','checkbox');
+	
+	$('#list').append('<li><input class="class_input_checkbox" type="checkbox"><span class="class_span">This is the text in new element. (with jQuery)</span></li>');
+});
