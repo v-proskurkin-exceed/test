28e8f6fc2191139b4f4bbaa40a71595c5361a46d
Filin <v.proskurkin.exceedteam@gmail.com>
2020-03-16 12:38:57 +0300

    Add a random color list

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-function getRandom(max) {
-	return Math.floor(Math.random() * max);
-}
-$("#but").on('click', function(){
-	
-	var text = $('.getting_text').val();
-	$('#list').append('<li><div class="cl1"><input class="class_input_checkbox" type="checkbox"></div><span></span></li>');
-	$('span').text(text);
-});
-let a = getRandom(4);
 
-alert(a);
-// $(function (){
-  
-// 		$('#but').click(function(){
+$(window).ready(function() {
 	
-// 		   var text = $('.getting_text').val();
-// 		   $('#list').append('<li><div class="cl1"><input class="class_input_checkbox" type="checkbox"></div><span class="class_span"></span></li>');
-// 		   $('span').text(text);
-			
-// 		});
-	
-// 	});
-// $(function (){
-	
-// 		$('but').click(function(){
-	
-// 		   var text = $('getting_text').val();
-	
-// 		   $('dv').text(text);
-	
-// 		});
-	
-// 	});
-	
+	$("#but").on('click', function(){
+		let arr = ['red','pink','purple','light_blue','light_aqua','navy_yellow'];
+		let rand = Math.floor(Math.random() * arr.length);
+		let text = $('.getting_text').val();
+		
+		$('#list').append('<li><div class="cl1"><input class="class_input_checkbox" type="checkbox"></div>'+'<span>'+ text + '</span>'+'</li>');
+		$('span:last').addClass(arr[rand]);
+		$('.cl1:last').addClass(arr[rand]);
+		$('.getting_text').val('');
+	});
+
+
+})
+// let arr = ['red','pink','purple','light_blue','light_aqua','navy_yellow'];
+// 	let rand = Math.floor(Math.random() * arr.length);
+// $('sp').addClass('sp '+arr[rand])
+
+
+
+
